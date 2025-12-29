ed370c937ef66c0c4f166d9b8d17ee0ff6868943
Goodnessmbakara <amicablembakara50@gmail.com>
2025-12-29 23:13:37 +0100

    fix: Update contract helpers to use openContractCall API
    - Replace makeContractCall with openContractCall for proper wallet integration
    - Fix TypeScript errors with network imports
    - Ensure all contract calls use Stacks Connect modal
    - Production build now successful

--- frontend/src/lib/contractHelpers.ts
+++ frontend/src/lib/contractHelpers.ts
@@ -1,13 +1,11 @@
+import { openContractCall } from '@stacks/connect';
 import {
-  makeContractCall,
-  broadcastTransaction,
-  AnchorMode,
   stringAsciiCV,
   uintCV,
   PostConditionMode,
+  AnchorMode,
 } from '@stacks/transactions';
-import { StacksMainnet, StacksTestnet } from '@stacks/network';
-import { UserSession } from '@stacks/connect';
+import { StacksTestnet, StacksMainnet } from '@stacks/network';
 
 const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS || 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
 const CONTRACT_NAME = 'bitvault-core';
@@ -19,7 +17,7 @@ const getNetwork = () => {
 };
 
 export const createChallenge = async (
-  userSession: UserSession,
+  userSession: any,
   title: string,
   targetAmount: number,
   depositFrequency: number,
@@ -28,123 +26,128 @@ export const createChallenge = async (
 ) => {
   const network = getNetwork();
   
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: 'create-challenge',
-    functionArgs: [
-      stringAsciiCV(title),
-      uintCV(targetAmount),
-      uintCV(depositFrequency),
-      uintCV(durationBlocks),
-      uintCV(maxParticipants),
-    ],
-    senderKey: userSession.loadUserData().appPrivateKey,
-    validateWithAbi: true,
-    network,
-    anchorMode: AnchorMode.Any,
-    postConditionMode: PostConditionMode.Deny,
-  };
-
-  const transaction = await makeContractCall(txOptions);
-  const broadcastResponse = await broadcastTransaction(transaction, network);
-  
-  return broadcastResponse;
+  return new Promise((resolve, reject) => {
+    openContractCall({
+      network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: CONTRACT_ADDRESS,
+      contractName: CONTRACT_NAME,
+      functionName: 'create-challenge',
+      functionArgs: [
+        stringAsciiCV(title),
+        uintCV(targetAmount),
+        uintCV(depositFrequency),
+        uintCV(durationBlocks),
+        uintCV(maxParticipants),
+      ],
+      postConditionMode: PostConditionMode.Deny,
+      onFinish: (data) => {
+        resolve(data);
+      },
+      onCancel: () => {
+        reject(new Error('Transaction cancelled by user'));
+      },
+    });
+  });
 };
 
 export const joinChallenge = async (
-  userSession: UserSession,
+  userSession: any,
   challengeId: number
 ) => {
   const network = getNetwork();
   
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: 'join-challenge',
-    functionArgs: [uintCV(challengeId)],
-    senderKey: userSession.loadUserData().appPrivateKey,
-    validateWithAbi: true,
-    network,
-    anchorMode: AnchorMode.Any,
-    postConditionMode: PostConditionMode.Deny,
-  };
-
-  const transaction = await makeContractCall(txOptions);
-  const broadcastResponse = await broadcastTransaction(transaction, network);
-  
-  return broadcastResponse;
+  return new Promise((resolve, reject) => {
+    openContractCall({
+      network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: CONTRACT_ADDRESS,
+      contractName: CONTRACT_NAME,
+      functionName: 'join-challenge',
+      functionArgs: [uintCV(challengeId)],
+      postConditionMode: PostConditionMode.Deny,
+      onFinish: (data) => {
+        resolve(data);
+      },
+      onCancel: () => {
+        reject(new Error('Transaction cancelled by user'));
+      },
+    });
+  });
 };
 
 export const makeDeposit = async (
-  userSession: UserSession,
+  userSession: any,
   challengeId: number,
   amount: number
 ) => {
   const network = getNetwork();
   
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: 'make-deposit',
-    functionArgs: [uintCV(challengeId), uintCV(amount)],
-    senderKey: userSession.loadUserData().appPrivateKey,
-    validateWithAbi: true,
-    network,
-    anchorMode: AnchorMode.Any,
-    postConditionMode: PostConditionMode.Deny,
-  };
-
-  const transaction = await makeContractCall(txOptions);
-  const broadcastResponse = await broadcastTransaction(transaction, network);
-  
-  return broadcastResponse;
+  return new Promise((resolve, reject) => {
+    openContractCall({
+      network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: CONTRACT_ADDRESS,
+      contractName: CONTRACT_NAME,
+      functionName: 'make-deposit',
+      functionArgs: [uintCV(challengeId), uintCV(amount)],
+      postConditionMode: PostConditionMode.Deny,
+      onFinish: (data) => {
+        resolve(data);
+      },
+      onCancel: () => {
+        reject(new Error('Transaction cancelled by user'));
+      },
+    });
+  });
 };
 
 export const completeChallenge = async (
-  userSession: UserSession,
+  userSession: any,
   challengeId: number
 ) => {
   const network = getNetwork();
   
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: 'complete-challenge',
-    functionArgs: [uintCV(challengeId)],
-    senderKey: userSession.loadUserData().appPrivateKey,
-    validateWithAbi: true,
-    network,
-    anchorMode: AnchorMode.Any,
-    postConditionMode: PostConditionMode.Deny,
-  };
-
-  const transaction = await makeContractCall(txOptions);
-  const broadcastResponse = await broadcastTransaction(transaction, network);
-  
-  return broadcastResponse;
+  return new Promise((resolve, reject) => {
+    openContractCall({
+      network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: CONTRACT_ADDRESS,
+      contractName: CONTRACT_NAME,
+      functionName: 'complete-challenge',
+      functionArgs: [uintCV(challengeId)],
+      postConditionMode: PostConditionMode.Deny,
+      onFinish: (data) => {
+        resolve(data);
+      },
+      onCancel: () => {
+        reject(new Error('Transaction cancelled by user'));
+      },
+    });
+  });
 };
 
 export const claimRewards = async (
-  userSession: UserSession,
+  userSession: any,
   challengeId: number
 ) => {
   const network = getNetwork();
   
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: 'claim-rewards',
-    functionArgs: [uintCV(challengeId)],
-    senderKey: userSession.loadUserData().appPrivateKey,
-    validateWithAbi: true,
-    network,
-    anchorMode: AnchorMode.Any,
-    postConditionMode: PostConditionMode.Deny,
-  };
-
-  const transaction = await makeContractCall(txOptions);
-  const broadcastResponse = await broadcastTransaction(transaction, network);
-  
-  return broadcastResponse;
+  return new Promise((resolve, reject) => {
+    openContractCall({
+      network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: CONTRACT_ADDRESS,
+      contractName: CONTRACT_NAME,
+      functionName: 'claim-rewards',
+      functionArgs: [uintCV(challengeId)],
+      postConditionMode: PostConditionMode.Deny,
+      onFinish: (data) => {
+        resolve(data);
+      },
+      onCancel: () => {
+        reject(new Error('Transaction cancelled by user'));
+      },
+    });
+  });
 };
